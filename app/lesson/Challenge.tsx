624e417f81a303bd15565890151426d8ecef882f
haroune22 <harounehamenniche123@gmail.com>
2024-08-18 16:25:07 +0100

    challenge cards & footer

---
+++ app/lesson/Challenge.tsx
@@ -0,0 +1,46 @@
+import { challengeOptions, challenges } from "@/db/schema"
+import { cn } from "@/lib/utils";
+import { Card } from "./Card";
+
+
+type Props = {
+    options: (typeof challengeOptions.$inferSelect)[];
+    onSelect: ( id: number ) => void;
+    status: "correct" | "wrong" | "none" ;
+    selectOption?: number;
+    disabled: boolean;
+    type: typeof challenges.$inferSelect["type"]
+}
+
+export const Challenge = ({
+    disabled,
+    onSelect,
+    options,
+    selectOption,
+    status,
+    type
+}:Props) => {
+  return (
+    <div className={cn(
+        'grid gap-2',
+        type === "ASSIST" && "grid-cols-1",
+        type === "SELECT" && "grid-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(0,1fr))] ",
+    )}>
+        {options.map((option, i) => (
+            <Card
+                key={option.id}
+                id={option.id}
+                text={option.text}
+                imageSrc={option?.imageSrc}
+                shortcut={`${i  + 1}`}
+                selected={selectOption === option.id}
+                onClick={()=> onSelect(option.id) }
+                status={status}
+                audioSrc={option.audioSrc}
+                disabled={disabled}
+                type={type}
+            />
+        ))}
+    </div>
+  )
+}
--- app/lesson/Quiz.tsx
+++ app/lesson/Quiz.tsx
@@ -3,6 +3,9 @@
 import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import { Header } from "./Header";
+import { QuestionBubble } from "./QuestionBubble";
+import { Challenge } from "./Challenge";
+import { Footer } from "./Footer";
 
 
 
@@ -27,8 +30,31 @@ export const Quiz = ({
     userSubscription,
 }:Props) => {
 
-    const [hearts, setHearts] = useState(initialHearts)
-    const [percentage, setPercentage] = useState(initialPercentage)
+    const [hearts, setHearts] = useState(initialHearts);
+    const [percentage, setPercentage] = useState(initialPercentage);
+    const [challenges] = useState(initialLessonChallenges);
+    const [activeIndex, setActiveIndex] = useState(()=> {
+        const uncompletedIndex = challenges.findIndex(c => !c.completed)
+        return uncompletedIndex === -1 ? 0 : uncompletedIndex
+    });
+
+    const [selectedOption, setSelectedOption ]= useState<number>();
+    const [status, setStatus ]= useState<"correct" | "wrong" | "none">("none");
+
+    const challenge = challenges[activeIndex]
+    const Options = challenge.challengeOptions;
+    
+
+    const onSelect = (id:number) => {
+        if(status !== "none") return;
+
+        setSelectedOption(id)
+
+    };
+
+    const title = challenge.type === "ASSIST" 
+    ? "Select the correct meaning" 
+    : challenge.question;
 
   return (
     <>
@@ -36,6 +62,33 @@ export const Quiz = ({
             hearts={hearts}
             percentage={percentage}
             hasActiveSubscription={!!userSubscription?.isActive}
+        />
+        <div className="flex-1">
+            <div className="h-full flex items-center justify-center">
+                <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12 ">
+                    <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+                        {title}
+                    </h1>
+                    <div className="">
+                        {challenge.type === "ASSIST" && (
+                            <QuestionBubble question={challenge.question} />
+                        )}
+                        <Challenge 
+                            options={Options}
+                            onSelect={onSelect}
+                            status={status}
+                            selectOption={selectedOption}
+                            disabled={false}
+                            type={challenge.type}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Footer
+            disabled={!selectedOption}
+            status={status}
+            onCheck={()=>{}}
         />
     </>
   )
